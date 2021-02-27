1fef303cc1351f850532db8c7eb01ff202e2355c
jlgge <jlgge@angkor>
2021-02-27 14:01:53 -0600

    Hellow World page for assignment

---
+++ webproject-Term-Project-Gallagher/src/com/unojg/Recipe.java
@@ -0,0 +1,46 @@
+package com.unojg;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/Recipe")
+public class Recipe extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Recipe() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		/*
+		* Business logics
+		*/
+		response.getWriter().append("Hello World!");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+	}
+
+}
+	
